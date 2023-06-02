27c5d393accc1867f300400066ea38d0e2464888
Muhammad Husni <121261240+itshusni29@users.noreply.github.com>
2023-06-02 23:17:59 +0700

    models.py

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 
+
 class Product(models.Model):
     GENDER = (
         ('M', 'M'),
@@ -18,6 +19,8 @@ class Product(models.Model):
     gender = models.CharField(max_length=1, null=True, choices=GENDER)
     notes = models.TextField(max_length=500, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    pdf = models.FileField(upload_to='media/pdf_files/', null=True, blank=True)
+
 
     
     def __str__(self):
