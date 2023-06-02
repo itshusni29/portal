1389cb3ed4c6a131d119e85fb47936d11aca24e2
Muhammad Husni <121261240+itshusni29@users.noreply.github.com>
2023-06-02 23:18:24 +0700

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, re_path
 from . import views
+from .views import edit_product
+
 
 urlpatterns = [
     path('index', views.index, name='index'),
@@ -7,7 +9,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('add_product', views.add_product, name='add_product'),
     path('product/<str:product_id>/', views.product, name='product'),
-    path('product/edit/<str:product_id>/', views.edit_product, name='edit_product'),
+    path('product/<int:product_id>/edit/', edit_product, name='edit_product'),
     path('delete_product/<str:product_id>/', views.delete_product, name='delete_product'),
     re_path(r'^.*\.*', views.pages, name='pages'),
 ]
